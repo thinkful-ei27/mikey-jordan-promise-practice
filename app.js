92152889b6a28fa2c7b8ace8ede6bd560a6aefa0
Jordan Heffernan <jheff2@gmail.com>
2018-12-20 09:55:26 -0800

    began promise all functions

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ const URL = 'https://swapi.co/api/people';
 const searchTerm = process.argv[2];
 // const test ="jar jar"
 
+const filmtitle =[];
+
+// const p = new Promise((resolve, reject) => {
+//   return axios.get(`${URL}/?search=${searchTerm}`);
+// });
+
 axios.get(`${URL}/?search=${searchTerm}`)
-  .then(res => console.log(res.data.results[0].films))
+  .then(res => {
+    const films = res.data.results[0].films;
+    return films;})
+  .then(films => {
+    const promise1 = function (films) {films.map(film => {
+      axios.get(`${film}`)
+        .then(res => {
+          return res.data.title;
+        });
+    });
+    };
+  })
+  .then(Promise.all(...promise1)) 
   .catch((error)=> console.log(error.message ,'error'));
 
