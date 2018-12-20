baefe4926ec61b1f29bccd797682f75718f6e410
mikeymanoguerra <mikeymanoguerra@gmail.com>
2018-12-20 14:25:51 -0500

    wa;lktrough

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ const searchTerm = process.argv[2];
 // const test ="jar jar"
 
 axios.get(`${URL}/?search=${searchTerm}`)
-  .then(res => console.log(res.data.results[0].films))
-  .catch((error)=> console.log(error.message ,'error'));
+  .then(res => {
+    const filmsArray  = res.data.results[0].films;
+    
+    const promises = filmsArray.map(filmUrl => axios.get(filmUrl));
+    return Promise.all(promises);
+  })
+  .then(responses => {
+    let films = responses.map(response =>{
+      return{ title: response.data.title, releaseDAte: response.data.release_date };
+    });
+    console.log(films);
+  })
+  .catch((error) => console.log(error.message ,'error'));
 
+
+
+
+
+//   .then(films => films.forEach(filmURL => {
+//     axios.get(`${filmURL}`)
+//       .then(res => {
+//         titleArray.push(res.data.title);
+//       });
+//   }))
+//   .catch((error) => console.log(error.message ,'error'));
+
+
+// Promise.all(filmLinkArray)
+//   .then(function(values){ console.log(values);});
+  
